f6d47b854f5681163904645db7666a6a43eeb05e
yoshuadupin <djosue@unitec.edu>
2017-01-27 17:37:56 -0600

    Ultimo bye

--- Lab2-DiegoMendoza.cpp
+++ Lab2-DiegoMendoza.cpp
@@ -4,17 +4,37 @@ using namespace std;
 
 void  ordenarArreglo(int );
 void cubos(int);
-
+void tercerEjercicio();
 
 int main(){
+	int opcion;
+	cout<<"Ingrese el ejercicio que desea ve\n1.Ordenar Arreglo	\n	2.Cubos de los numeros\n3.Ejercicio de ver que hace:";
+	cin>>opcion;
 	int n; 
-	 cout<<"Ingrese el tamano:";
-	cin>>n; 
+	 
    // ordenarArreglo( n);
-	cubos(n); 
+	switch(opcion){
+		case 1:
+			cout<<"Ingrese el tamano del arreglo:";
+			cin>>n;
+			ordenarArreglo(n);
+			break;
+		case 2:
+			cout<<"Ingrese la cantidad de cubos que quieren ver:";
+			cin>>n;
+			cubos(n);
+
+			break;
+		case 3:
+			cout<<"Ejercicio 3:";
+		default:
+			cout<<"bye bye";
+			
+	}
+
 }
 
-
+//Segundo Ejercicio
 void cubos(int n ){
 	
 		int cont=1;
@@ -65,4 +85,35 @@ void ordenarArreglo( int n){
    cout<<"La diferencia entre estos dos"<<arreglo[n-1]-arreglo[0];
    
 }
+//Tercer ejercicio
+void tercerEjercicio(){
+ int n, m;
 
+ do {
+
+   cin >> n >> m;
+
+ } while ((n < 0) || (m < 0));
+//Los numeros deben ser mayores de 0 para salir del ciclo
+ 
+
+ int x = 0;
+
+ 
+//Mientras sea distinto de 0
+ while (m != 0) {
+
+   if (m % 2 != 0)
+	   x = x + n;//Suma el primer numero a X
+	
+	n = n + n;//Suma dos veces el primer numeo a n
+	m = m / 2;//A m lo divide entre dos
+
+ 	}
+ 
+
+ 
+
+ cout << "El resultado es " << x << endl;//muestra el resultado que  es la suma del primer numero con x que es 0;
+}
+ // Fin del código
